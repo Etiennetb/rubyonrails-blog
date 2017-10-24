d4e1c6f494860f8a807d03772090ced49ede6782
Etienne Bouron <etienne.toutbien@gmail.com>
2017-10-24 15:03:38 +0200

    tuto done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,35 @@
 class ArticlesController < ApplicationController
 
 	def new
+		@article = Article.new
 	end
 
 	def create
 		@article = Article.new(article_params)
-		@article.save
+		if @article.save
 		redirect_to @article
+		else
+			render "new"
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy 
+			redirect_to articles_path
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render "edit"
+		end
 	end
 
 	def show
@@ -22,7 +45,7 @@ class ArticlesController < ApplicationController
 	private
 
 	def article_params
-		params.require(:article).permit(:title, :text)
+		params.permit(:title, :text)
 	end
 
 
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = Comment.new(comment_params)
+		@comment.article = @article
+		@comment.save
+		redirect_to article_path(@article)
+	end
+	
+	private
+
+	def comment_params
+		params.permit(:commenter, :body)
+	end
+end
